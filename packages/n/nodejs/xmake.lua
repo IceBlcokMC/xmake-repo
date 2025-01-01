f5a1808f56c660e11e500c008aa84b016f6564c4
engsr6982 <pwxg286704@163.com>
2025-01-01 13:16:39 +0800

    📦(node.js):update v23.5.0

--- packages/n/nodejs/xmake.lua
+++ packages/n/nodejs/xmake.lua
@@ -2,6 +2,7 @@ package("nodejs")
     add_urls("https://github.com/IceBlcokMC/node/releases/download/v$(version)/sdk.zip")
 
     add_versions("16.20.2", "b4fd8ca7236237c00e8c17288381d9336b64578f04e626f337472f95280278e6")
+    add_versions("23.5.0", "a236175ff33698039217d1d94b8e98427d92275197550a112f27e91106807cf4")
 
     add_includedirs("include/")
     add_includedirs("include/v8/")
