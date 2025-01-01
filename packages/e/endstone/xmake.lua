6da6437c74e1eeb4c85a91a876062763a147efbf
engsr6982 <pwxg286704@163.com>
2025-01-01 13:56:46 +0800

    📦(endstone): update v0.5.7.1

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -1,16 +1,33 @@
+local CXX20Patch = "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/cxx20.patch"
+local CXX20PatchSha256 = "547ae3d325b8deb68747179b6bc3aa8772ba4efe36263bf31f34be7a3aac2ceb"
+
+local LinuxPatch = "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/linux.patch"
+local LinuxPatchSha256 = "1e7c6a961abf821803b42dcd43d1c88524caa8012e333b9e47ec6faa00037c74"
+
+local PatchVersions = {
+    ["0.5.6"] = {CXX20Patch, LinuxPatch},
+    ["0.5.7.1"] = {CXX20Patch, LinuxPatch}
+}
+
+
 package("endstone")
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/EndstoneMC/endstone")
     set_description("Endstone - High-level Plugin API for Bedrock Dedicated Servers (BDS), in both Python and C++")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/EndstoneMC/endstone/archive/refs/tags/v$(version).tar.gz","https://github.com/EndstoneMC/endstone.git")
-    add_versions("0.5.6","2a991d4009025a10c4469f160a15ad40594f82ebd08dab510ea0f42d61b7c8dd")
-    add_patches("0.5.6", "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/cxx20.patch",
-                        "547ae3d325b8deb68747179b6bc3aa8772ba4efe36263bf31f34be7a3aac2ceb")
-    if (is_plat("linux")) then 
-        add_patches("0.5.6", "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/linux.patch",
-                        "1e7c6a961abf821803b42dcd43d1c88524caa8012e333b9e47ec6faa00037c74")   
+    add_urls("https://github.com/EndstoneMC/endstone/archive/refs/tags/v$(version).tar.gz", "https://github.com/EndstoneMC/endstone.git")
+
+
+    add_versions("0.5.6", "2a991d4009025a10c4469f160a15ad40594f82ebd08dab510ea0f42d61b7c8dd")
+    add_versions("0.5.7.1", "956d8f853d2d7d00844b8d9ac73afc35ef549c0405fc09c22fedf937c9fbe1d6")
+
+    -- Add patches for CXX20 and Linux
+    for version, patches in pairs(PatchVersions) do
+        add_patches(version, patches[1], CXX20PatchSha256)
+        if is_plat("linux") then
+            add_patches(version, patches[2], LinuxPatchSha256)
+        end
     end
 
     on_install("windows", "linux", function (package)
