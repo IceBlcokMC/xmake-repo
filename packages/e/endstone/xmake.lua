c75c14f1e95f566f9d3e957b23753396a1896a50
engsr6982 <pwxg286704@163.com>
2025-01-13 18:19:19 +0800

    📦(endstone): add patches

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -1,14 +1,8 @@
-local CXX20Patch = "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/cxx20.patch"
+local CXX20Patch = "patches/cxx20.patch"
 local CXX20PatchSha256 = "547ae3d325b8deb68747179b6bc3aa8772ba4efe36263bf31f34be7a3aac2ceb"
 
-local LinuxPatch = "https://raw.githubusercontent.com/engsr6982/Js_Engine/refs/heads/develop/patch/linux.patch"
+local LinuxPatch = "patches/linux.patch"
 local LinuxPatchSha256 = "1e7c6a961abf821803b42dcd43d1c88524caa8012e333b9e47ec6faa00037c74"
-
-local PatchVersions = {
-    ["0.5.6"] = {CXX20Patch, LinuxPatch},
-    ["0.5.7.1"] = {CXX20Patch}
-}
-
 
 package("endstone")
     set_kind("library", {headeronly = true})
@@ -18,19 +12,12 @@ package("endstone")
 
     add_urls("https://github.com/EndstoneMC/endstone/archive/refs/tags/v$(version).tar.gz", "https://github.com/EndstoneMC/endstone.git")
 
-
     add_versions("0.5.6", "2a991d4009025a10c4469f160a15ad40594f82ebd08dab510ea0f42d61b7c8dd")
-    add_versions("0.5.7.1", "956d8f853d2d7d00844b8d9ac73afc35ef549c0405fc09c22fedf937c9fbe1d6")
+    add_patches("0.5.6", LinuxPatch, LinuxPatchSha256)
+    add_patches("0.5.6", CXX20Patch, CXX20PatchSha256)
 
-    -- Add patches for CXX20 and Linux
-    for version, patches in pairs(PatchVersions) do
-        add_patches(version, patches[1], CXX20PatchSha256)
-        if is_plat("linux") then
-            if #patches > 1 then
-                add_patches(version, patches[2], LinuxPatchSha256)
-            end
-        end
-    end
+    add_versions("0.5.7.1", "956d8f853d2d7d00844b8d9ac73afc35ef549c0405fc09c22fedf937c9fbe1d6")
+    add_patches("0.5.7.1", CXX20Patch, CXX20PatchSha256)
 
     on_install("windows", "linux", function (package)
         os.cp("include", package:installdir())
