7965c040338aac0d74bda1b1c12e4551f98f39cc
engsr6982 <pwxg286704@163.com>
2025-02-03 21:40:26 +0800

    feat(nodejs): add uv config to enable/disable uv

--- packages/n/nodejs/xmake.lua
+++ packages/n/nodejs/xmake.lua
@@ -5,9 +5,18 @@ package("nodejs")
     add_versions("22.12.0", "c957c1374293364a25adbb05af4326f688b5abf7a87bbd681946df72187ea3a1")
     add_versions("23.5.0", "a236175ff33698039217d1d94b8e98427d92275197550a112f27e91106807cf4")
 
+    add_configs("uv", {description = "Enable nodejs libuv", default = true, type = "boolean"})
+
     add_includedirs("include/")
     add_includedirs("include/v8/")
-    add_includedirs("include/uv/")
+
+    -- add_includedirs("include/uv/")
+    on_load(function (package)
+        if package:config("uv") then
+            package:add("includedirs", "include/uv/")
+        end
+    end)
+
     on_install(function (package)
         os.cp("*", package:installdir())
     end)
