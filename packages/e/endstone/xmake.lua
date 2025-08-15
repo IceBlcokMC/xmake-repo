1a35a18d8ecf6122f63aa53d6d1720d70135e26e
engsr6982 <pwxg286704@163.com>
2025-08-15 17:34:33 +0800

    build: update endstone package and remove unnecessary patches

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -1,26 +1,13 @@
-local CXX20Patch = "patches/cxx20.patch"
-local CXX20PatchSha256 = "547ae3d325b8deb68747179b6bc3aa8772ba4efe36263bf31f34be7a3aac2ceb"
-
-local LinuxPatch = "patches/linux.patch"
-local LinuxPatchSha256 = "1e7c6a961abf821803b42dcd43d1c88524caa8012e333b9e47ec6faa00037c74"
-
 package("endstone")
     set_kind("library", {headeronly = true})
     set_homepage("https://github.com/EndstoneMC/endstone")
     set_description("Endstone - High-level Plugin API for Bedrock Dedicated Servers (BDS), in both Python and C++")
     set_license("Apache-2.0")
 
-    add_urls("https://github.com/EndstoneMC/endstone/archive/refs/tags/v$(version).tar.gz", "https://github.com/EndstoneMC/endstone.git")
+    add_urls("https://github.com/EndstoneMC/endstone.git")
 
-    add_versions("0.5.6", "2a991d4009025a10c4469f160a15ad40594f82ebd08dab510ea0f42d61b7c8dd")
-    add_patches("0.5.6", LinuxPatch, LinuxPatchSha256)
-    add_patches("0.5.6", CXX20Patch, CXX20PatchSha256)
-
-    add_versions("0.5.7.1", "956d8f853d2d7d00844b8d9ac73afc35ef549c0405fc09c22fedf937c9fbe1d6")
-    add_patches("0.5.7.1", CXX20Patch, CXX20PatchSha256)
-
-    add_versions("0.6.0", "d9d7f865629165230b6dfcb34b359186524f6784a7655f39a7140ca998455a38")
-    add_versions("0.7.1", "fb1324eae85ba2e73c1bff58c5cabe4a0821fb7b4ca9249fe31aa9c9e74a7aee")
+    -- <version, commit>
+    add_versions("0.10.4", "9a96f5d2ca2cab7aecf9d5fd5ff1d863accca9f0")
 
     on_install("windows", "linux", function (package)
         os.cp("include", package:installdir())
