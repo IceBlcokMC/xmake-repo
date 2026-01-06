6669bf94a45df5c27afec361c12731c9af8d56ec
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2026-01-06 15:47:01 +0800

    fix(perm_core): try fix fmt odr error

--- packages/p/perm_core/xmake.lua
+++ packages/p/perm_core/xmake.lua
@@ -2,6 +2,8 @@ package("perm_core")
     set_license("LGPL-3.0")
     set_homepage("https://github.com/IceBlcokMC/PermCore")
 
+    add_defines("FMT_HEADER_ONLY=1") -- fix ODR
+
     -- <Version, CommitHash>
     add_urls("https://github.com/IceBlcokMC/PermCore.git")
 
