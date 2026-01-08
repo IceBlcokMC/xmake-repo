cd66acf271e9fecc607955a3a6472a37a5e9ce5e
engsr6982 <pwxg286704@163.com>
2026-01-08 22:28:41 +0800

    build: add ll-bstats package configuration

---
+++ packages/l/ll-bstats/xmake.lua
@@ -0,0 +1,11 @@
+package("ll-bstats")
+    set_license("MIT")
+    set_homepage("https://github.com/IceBlcokMC/ll-bstats")
+
+    -- <Version, CommitHash>
+    add_urls("https://github.com/IceBlcokMC/ll-bstats.git")
+    add_versions("0.1.0", "76b1a55caf625922d60818424a3467c5c9c7ef80")
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package)
+    end)
