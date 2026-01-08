7af5f0b8e14006f31f002f2c4bf54743becfe239
engsr6982 <pwxg286704@163.com>
2026-01-08 22:31:24 +0800

    build: add cpr dependency with ssl support

--- packages/l/ll-bstats/xmake.lua
+++ packages/l/ll-bstats/xmake.lua
@@ -6,6 +6,8 @@ package("ll-bstats")
     add_urls("https://github.com/IceBlcokMC/ll-bstats.git")
     add_versions("0.1.0", "76b1a55caf625922d60818424a3467c5c9c7ef80")
 
+    add_deps("cpr[ssl=y] 1.12.0")
+
     on_install(function (package)
         import("package.tools.xmake").install(package)
     end)
