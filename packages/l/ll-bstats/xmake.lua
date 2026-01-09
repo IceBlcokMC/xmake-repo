074d7513110bcf85593dcb071ac8764752c53677
engsr6982 <pwxg286704@163.com>
2026-01-09 23:25:29 +0800

    build: add version 0.1.2 for ll-bstats

--- packages/l/ll-bstats/xmake.lua
+++ packages/l/ll-bstats/xmake.lua
@@ -6,6 +6,7 @@ package("ll-bstats")
     add_urls("https://github.com/IceBlcokMC/ll-bstats.git")
     add_versions("0.1.0", "76b1a55caf625922d60818424a3467c5c9c7ef80")
     add_versions("0.1.1", "b3e7d663d24d401ac24a9c5e2e448e94efb4d28e")
+    add_versions("0.1.2", "339f0d17fbca57ff0b1e5573bc61700213a7916f")
 
     add_deps("cpr[ssl=y] 1.12.0")
 
