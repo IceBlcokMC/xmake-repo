7a7f0b9c32230ed5ba1a44c17dd72037f2914e25
engsr6982 <pwxg286704@163.com>
2026-01-25 23:33:53 +0800

    build(debug_shape): add versions 0.5.0 and 0.6.0

--- packages/d/debug_shape/xmake.lua
+++ packages/d/debug_shape/xmake.lua
@@ -7,6 +7,8 @@ package("debug_shape")
     add_versions("0.1.0", "5b5e740fa130985bc14c615836e2a0dda9a97e7c")
     add_versions("0.2.0", "7c337b5fd6b0c55c03010f535697655ee3d477b5")
     add_versions("0.3.0", "6de0232c33223750bf254691350acd7837c47d42")
+    add_versions("0.5.0", "0efe5753632b8430e34b3f553748050f9acef3fe")
+    add_versions("0.6.0", "56165e30d12d3b3a786e3812d87296ac476dc6de")
 
     on_install(function (package)
         import("package.tools.xmake").install(package)
