d771301f9a27faa0ec1ed0006dd57d59cb6bd51a
engsr6982 <pwxg286704@163.com>
2026-01-25 23:46:43 +0800

    build: add version 0.2.0 for ll-bstats

--- packages/l/ll-bstats/xmake.lua
+++ packages/l/ll-bstats/xmake.lua
@@ -7,6 +7,7 @@ package("ll-bstats")
     add_versions("0.1.0", "76b1a55caf625922d60818424a3467c5c9c7ef80")
     add_versions("0.1.1", "b3e7d663d24d401ac24a9c5e2e448e94efb4d28e")
     add_versions("0.1.2", "339f0d17fbca57ff0b1e5573bc61700213a7916f")
+    add_versions("0.2.0", "59d46ef6b4bb5a15d04509adafb6760fd54ac49a")
 
     add_deps("cpr[ssl=y] 1.12.0")
 
