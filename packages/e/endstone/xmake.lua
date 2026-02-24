6d0051cf17523b605e877ca8f5f595ed693bb54b
engsr6982 <pwxg286704@163.com>
2026-02-24 14:11:10 +0800

    build(endstone): add version 0.11.1

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -1,5 +1,5 @@
 package("endstone")
-    set_kind("library", {headeronly = true})
+    set_kind("library", { headeronly = true })
     set_homepage("https://github.com/EndstoneMC/endstone")
     set_description("Endstone - High-level Plugin API for Bedrock Dedicated Servers (BDS), in both Python and C++")
     set_license("Apache-2.0")
@@ -8,6 +8,7 @@ package("endstone")
 
     -- <version, commit>
     add_versions("0.10.4", "9a96f5d2ca2cab7aecf9d5fd5ff1d863accca9f0")
+    add_versions("0.11.1", "a8d0ac78023d830db68eddc08df49e7800271301")
 
     on_install("windows", "linux", function (package)
         os.cp("include", package:installdir())
