7948ca365c41de061055ab330fc03bcb0dab3eae
engsr6982 <pwxg286704@163.com>
2026-02-24 14:20:37 +0800

    build(libnode): add version 24.13.0 with platform-specific hashes

--- packages/l/libnode/xmake.lua
+++ packages/l/libnode/xmake.lua
@@ -1,5 +1,10 @@
 package("libnode")
-    add_versions("22.18.0", "a527dfa0d8cb423334b23637c8dda79793d2e2671b92364f7c341cf0bffd0999")
+    libnode_versions = {
+        ["24.13.0"] = {
+            ["win"] = "0599e607ffb89cf5389420e899f683f3e33d7881873d2cad8cd87cd033276a33",
+            ["linux"] = "3737ad2986d5e532b70421040f9af4bc0515172645c8843b623502e61381767d"
+        }
+    }
 
     add_includedirs("include/")
     add_includedirs("include/v8/")
@@ -17,8 +22,12 @@ package("libnode")
             return
         end
 
-        local url = "https://github.com/IceBlcokMC/libnode/releases/download/v"..package:version_str() .."/libnode-"..plat.."-x64-sdk.zip"
+        -- 遍历版本表添加对应平台版本
+        for k, v in pairs(libnode_versions) do
+            package:add("versions", k, v[plat])
+        end
 
+        local url = "https://github.com/IceBlcokMC/libnode/releases/download/v"..package:version_str() .."/libnode-"..plat.."-x64-sdk.zip"
         package:add("urls", url)
     end)
 
