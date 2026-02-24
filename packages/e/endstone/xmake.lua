68aa5bd0ff06cf565e703899cdc7dd7dd8180448
engsr6982 <pwxg286704@163.com>
2026-02-24 14:14:46 +0800

    build(endstone): add expected-lite dependency

--- packages/e/endstone/xmake.lua
+++ packages/e/endstone/xmake.lua
@@ -10,6 +10,8 @@ package("endstone")
     add_versions("0.10.4", "9a96f5d2ca2cab7aecf9d5fd5ff1d863accca9f0")
     add_versions("0.11.1", "a8d0ac78023d830db68eddc08df49e7800271301")
 
+    add_deps("expected-lite 0.8.0")
+
     on_install("windows", "linux", function (package)
         os.cp("include", package:installdir())
     end)
